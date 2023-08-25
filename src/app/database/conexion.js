35c8d92e501cb305a97556bca291c8e88dd661c5
wilfredo666 <31371350+wilfredo666@users.noreply.github.com>
2023-08-25 11:29:18 -0400

    082523

--- src/app/database/conexion.js
+++ src/app/database/conexion.js
@@ -1,16 +1,22 @@
 const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize("postgres://postgres:postgres@localhost/dbcatastro", {
+
+const sequelize = new Sequelize("postgres://postgres:administrador@ec2-100-24-63-57.compute-1.amazonaws.com:5432/dbcatastro", {
   dialect: 'postgres'
   // anything else you want to pass
 })
-/*const sequelize = new Sequelize(
-  process.env.DATABASE, 
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,
+
+/*
+
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.USER,
+  process.env.PASSWORD,
   {
     host: process.HOSTDEV,
     dialect: "postgres",
   }
-);*/
-
+);
+*/
 module.exports = sequelize;
+
+
